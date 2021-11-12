96c45305ee5b655157e395abc6d34554ad8101c4
WoodKT <soulsearcher14@gmail.com>
2021-11-12 03:10:59 -0500

    Update script.js
    functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,75 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+// const interface = 'audio';
+// const private = 'moo';
+
+function logger(){
+    console.log('My name is Kristy');
+}
+
+//calling/running/invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+//function declaration
+function calcAge1(birthYear){
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1998);
+console.log(age1);
+
+//function expression
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1988);
+console.log(age1, age2);
+
+//arrow function
+const calcAge3  = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1985);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+ //   return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1994, 'bob'));
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
